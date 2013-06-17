2212125ca7cb14df3afea77b47ec53a71e144675
Volodimir Musienko <volodymir.musienko@gmail.com>
2013-06-17 13:15:14 +0300

    forgot files...

---
+++ WashDepot/WDIncrementalStore.m
@@ -0,0 +1,22 @@
+#import "WDIncrementalStore.h"
+#import "WDAPIClient.h"
+
+@implementation WDIncrementalStore
+
++ (void)initialize {
+    [NSPersistentStoreCoordinator registerStoreClass:self forStoreType:[self type]];
+}
+
++ (NSString *)type {
+    return NSStringFromClass(self);
+}
+
++ (NSManagedObjectModel *)model {
+    return [[NSManagedObjectModel alloc] initWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"db" withExtension:@"xcdatamodeld"]];
+}
+
+- (id <AFIncrementalStoreHTTPClient>)HTTPClient {
+    return [WDAPIClient sharedClient];
+}
+
+@end
